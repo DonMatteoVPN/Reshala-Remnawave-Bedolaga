b454a41b7feb24b9a65422790ad06970b24a0ef7
DonMatteo <nikitindmitrij161@gmail.com>
2025-11-05 20:16:24 +0300

    Фикс ошибки

--- install_reshala.sh
+++ install_reshala.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 
 # --- КОНСТАНТЫ И ПЕРЕМЕННЫЕ ---
 readonly VERSION="v0.6"
-readonly SCRIPT_URL="hhttps://raw.githubusercontent.com/DonMatteoVPN/reshala-script/main/install_reshala.sh"
+readonly SCRIPT_URL="https://raw.githubusercontent.com/DonMatteoVPN/reshala-script/main/install_reshala.sh"
 CONFIG_FILE="${HOME}/.reshala_config"
 LOGFILE="/var/log/reshala_ops.log"
 INSTALL_PATH="/usr/local/bin/reshala"
